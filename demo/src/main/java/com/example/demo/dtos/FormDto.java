892cd3642ac05812005b57b759f64f0cdf912da1
Fabio Schitini <fabioschitini1@hotmail.com>
2023-09-26 16:29:45 -0300

    iniitial commit

--- demo/src/main/java/com/example/demo/dtos/FormDto.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.dtos;
-
-import com.example.demo.models.Categoria;
-
-public record FormDto( String titulo, String texto,Long usuario, Categoria categoria) {
-
-}
-
--- demo/src/main/java/com/example/demo/repositories/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.models.Usuarios;
-
-public interface UsuarioRepository extends  JpaRepository<Usuarios, Long>{
-
-}
